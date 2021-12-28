36e4e2402a21ebf84d1778eb840a5a31315d950f
nikolahuang <nikolahuang@tencent.com>
2021-12-28 19:36:57 +0800

    init

---
+++ src/main/java/com/tencent/code/ArraySubset.java
@@ -0,0 +1,72 @@
+package com.tencent.code;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ *
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ * 示例 2：
+ *
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+ * nums 中的所有元素 互不相同
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subsets
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class ArraySubset {
+    List<Integer> t = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(0, nums);
+        return ans;
+    }
+
+    /**
+     *
+     * @param cur 当前位置
+     * @param nums 数组
+     */
+    public void dfs(int cur, int[] nums) {
+        // 指针已遍历完数组
+        if (cur == nums.length) {
+            ans.add(new ArrayList<Integer>(t));
+            return;
+        }
+        // 当前位置的元素在数组
+        t.add(nums[cur]);
+        dfs(cur + 1, nums);
+        // 当前位置的元素不在数组
+        t.remove(t.size() - 1);
+        dfs(cur + 1, nums);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        ArraySubset subset = new ArraySubset();
+        System.out.println(subset.subsets(arr));
+    }
+}
